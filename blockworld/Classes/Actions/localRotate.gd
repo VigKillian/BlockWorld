a3b47112c0e04ae9564f0c05e45747796d03be7f
VigKillian <viguier.killian05@gmail.com>
2025-09-28 20:34:59 +0200

    Local transformations

---
+++ blockworld/Classes/Actions/localRotate.gd
@@ -0,0 +1,19 @@
+extends Action
+class_name ActionLocalRotate
+var ranges: Dictionary = {
+	"p_degree" : [0, 270, 90], # min, max, step
+	"p_axis" : [0, 2, 1]
+}
+@export var p_degree: int
+@export_enum("X", "Y", "Z") var p_axis: int
+
+func check_ranges():
+	p_degree = clamp(p_degree, ranges["p_degree"][0], ranges["p_degree"][1])
+	p_degree = snapped(p_degree, ranges["p_degree"][2])
+	p_axis = clamp(p_axis, ranges["p_axis"][0], ranges["p_axis"][1])
+
+func exec(agent: Agent):
+	check_ranges()
+	var axis = Vector3.ZERO
+	axis[p_axis] = 1
+	agent.rotate(axis, deg_to_rad(p_degree))
---
+++ blockworld/Classes/Actions/localTranslate.gd
@@ -0,0 +1,24 @@
+extends Action
+class_name ActionLocalTranslate
+var ranges: Dictionary = {
+	"p_delta" : [
+		Vector3i(-3, -1, -3),
+		Vector3i(3, 1, 3),
+		Vector3i(1, 1, 1)
+		]
+}
+
+@export var p_delta: Vector3i
+
+const scale_factor := 0.5
+
+func check_ranges():
+	p_delta = p_delta.clamp(
+		ranges["p_delta"][0],
+		ranges["p_delta"][1],
+	)
+	p_delta = p_delta.snapped(ranges["p_delta"][2])
+
+func exec(agent: Agent):
+	check_ranges()
+	agent.position += Vector3(p_delta) * scale_factor
